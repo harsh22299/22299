f2e3a0583f3baff2dbea1f17da55641ae2d71903
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-30 14:01:05 +1200

    ww

--- plane.py
+++ plane.py
@@ -15,12 +15,20 @@ def print_all_plane():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all results
+    print(f"aircraft              top_speed    manufacturer             year_intro   country_origin     plane_class ")
     for plane in results:
-        print(f'{plane[1]:<25}{plane[2]:<10}{plane[3]:<30}{plane[4]:<10}{plane[5]:<20}{plane[6]:<20}')
+        print(f'{plane[1]:<25}{plane[2]:<10}{plane[3]:<28}{plane[4]:<12}{plane[5]:<19}{plane[6]:<20}')
     #loop finishes here
     db.close()
 
 
 
 #main code 
-print_all_plane()
+while True:
+    user_input = input("\nWhat would you like to do?.\n1.Print all aircraft\n2.Exit\n")
+if user_input == "1":
+    print_all_plane()
+if user_input == "2":
+    pass
+else:
+    print("That is not an option\n")
