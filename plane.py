7e4345c599e762a63c4c8d864c333f079cf2facb
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-05 14:12:01 +1200

    ww

--- plane.py
+++ plane.py
@@ -22,13 +22,70 @@ def print_all_plane():
     db.close()
 
 
+def print_all_plane_sorted_speed():
+    '''print all the plane sorted by speed'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT *  FROM plane ORDER BY top_speed DESC"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all results
+    print(f"aircraft              top_speed    manufacturer             year_intro   country_origin     plane_class ")
+    for plane in results:
+        print(f'{plane[1]:<25}{plane[2]:<10}{plane[3]:<28}{plane[4]:<12}{plane[5]:<19}{plane[6]:<20}')
+    #loop finishes here
+    db.close()
+
+
+def print_all_plane_sorted_year_intro():
+    """print all aircraft sorted by year introduced"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM plane ORDER BY year_intro DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all results
+    print(f"aircraft              top_speed    manufacturer             year_intro   country_origin     plane_class ")
+    for plane in results:
+        print(f'{plane[1]:<25}{plane[2]:<10}{plane[3]:<28}{plane[4]:<12}{plane[5]:<19}{plane[6]:<20}')
+    #loop finishes here
+    db.close()
+
+
+def print_all_plane_from_fighter_class():
+    """print all aircraft from fighter class"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM plane WHERE plane_class = 'Fighter';"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all results
+    print(f"aircraft              top_speed    manufacturer             year_intro   country_origin     plane_class ")
+    for plane in results:
+        print(f'{plane[1]:<25}{plane[2]:<10}{plane[3]:<28}{plane[4]:<12}{plane[5]:<19}{plane[6]:<20}')
+    #loop finishes here
+    db.close()
 
 #main code 
 while True:
-    user_input = input("\nWhat would you like to do?.\n1.Print all aircraft\n2.Exit\n")
-if user_input == "1":
-    print_all_plane()
-if user_input == "2":
-    pass
-else:
-    print("That is not an option\n")
+    user_input = input(
+"""
+What would you like to do?
+1. Print all aircraft.
+2. Print all aircraft sorted by speed.
+3. Print all aircraft sorted by year introduced
+4. Print all aircraft from Fighter class
+5. Exit
+""")
+    if user_input == "1":
+        print_all_plane()
+    if user_input == "2":
+        print_all_plane_sorted_speed()
+    if user_input == "3":
+        print_all_plane_sorted_year_intro()
+    if user_input == "4":
+        print_all_plane_from_fighter_class()
+    if user_input == "5":
+        break 
+    else:
+        print("That is not an option\n")
